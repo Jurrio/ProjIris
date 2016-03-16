024317f4623cc5b6c0b1875141f8205040fc1266
Jurrio <jurimik@yandex.ua>
2016-03-16 08:04:41 +0200

    create class Person

---
+++ Salon/src/com/jurimik/model/person/Person.java
@@ -0,0 +1,48 @@
+package com.jurimik.model.person;
+
+public abstract class Person {
+	
+	private int id;
+	private String firstName;
+    private String lastName;
+    private String phone;
+    
+    public Person(String firstName, String lastName, String phone, String tableName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+    
+}
