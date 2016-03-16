6c4250772dbdb454a301e024038b23a8b3f0ff6a
Jurrio <jurimik@yandex.ua>
2016-03-16 14:32:29 +0200

    added parsing to Role

--- Salon/src/com/jurimik/util/Parser.java
+++ Salon/src/com/jurimik/util/Parser.java
@@ -1,9 +1,15 @@
 package com.jurimik.util;
 
+import com.jurimik.enums.Role;
+
 public class Parser {
 
 	public static boolean parseBoolean(String param) {
 		return param != null;
+	}
+
+	public static Role parseRole(String role) {
+		return Role.valueOf(role.toUpperCase());
 	}
 
 }
