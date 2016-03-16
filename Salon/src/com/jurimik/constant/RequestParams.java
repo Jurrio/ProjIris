b79f85baf98cd3866dc0e09a8779311ec26f0fe3
Jurrio <jurimik@yandex.ua>
2016-03-16 10:13:20 +0200

    added class for constants in web-page RequestParam.java

---
+++ Salon/src/com/jurimik/constant/RequestParams.java
@@ -0,0 +1,12 @@
+package com.jurimik.constant;
+
+public class RequestParams {
+	
+	public static final String type = "type";
+	public static final String firstName = "first-name";
+	public static final String lastName = "last-name";
+	public static final String phone = "phone";
+	public static final String isHaveCard = "is-have-card";
+	public static final String card = "card";
+	
+}
