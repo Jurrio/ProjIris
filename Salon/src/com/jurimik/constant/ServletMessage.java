216e1c616fab9f86a7ba6e9ff0e5676b68bc41cf
Jurrio <jurimik@yandex.ua>
2016-03-16 15:01:09 +0200

    created constant/ServletMessage.java

---
+++ Salon/src/com/jurimik/constant/ServletMessage.java
@@ -0,0 +1,9 @@
+package com.jurimik.constant;
+
+public class ServletMessage {
+
+	public static final String AddSuccess = "Person has been added succefully!";
+	public static final String AddCrush = "Person not been added in repository!";
+	
+	
+}
