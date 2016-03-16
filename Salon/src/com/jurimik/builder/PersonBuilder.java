d3b57e6367df94bed1eec2fb38323fc0110c12e1
Jurrio <jurimik@yandex.ua>
2016-03-16 11:52:16 +0200

    implemented PersonBuilder

---
+++ Salon/src/com/jurimik/builder/PersonBuilder.java
@@ -0,0 +1,53 @@
+package com.jurimik.builder;
+
+import com.jurimik.enums.Role;
+import com.jurimik.model.person.Person;
+import com.jurimik.util.factory.ClientCreator;
+import com.jurimik.util.factory.EmployeeCreator;
+
+public class PersonBuilder {
+	private Role role;
+	private final String firstName;
+	private final String lastName;
+	private String phone;
+	private String card;
+//	private String position;
+	public PersonBuilder(String role, String firstName, String lastName) {
+		this.role = Role.valueOf(role.toUpperCase());
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public void phone(String phone) {
+		this.phone = phone;
+	}
+	public void card(String card) {
+		this.card = card;
+	}
+	
+	public Role getRole() {
+		return role;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public String getCard() {
+		return card;
+	}
+	
+	public Person build() {
+		Person person;
+		if (role == Role.CLIENT)
+			person = new ClientCreator().factoryMethod(this);
+		else if (role == Role.EMPLOYEE) {
+			return new EmployeeCreator().factoryMethod(this);
+		}
+		return null;
+	}
+	
+}
