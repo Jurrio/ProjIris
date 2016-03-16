60b490e52bd0672262679630f0dd897e6e699fc6
Jurrio <jurimik@yandex.ua>
2016-03-16 11:27:45 +0200

    fixed constants name to UPPER CASE

--- Salon/src/com/jurimik/constant/RequestParams.java
+++ Salon/src/com/jurimik/constant/RequestParams.java
@@ -2,11 +2,11 @@ package com.jurimik.constant;
 
 public class RequestParams {
 	
-	public static final String type = "type";
-	public static final String firstName = "first-name";
-	public static final String lastName = "last-name";
-	public static final String phone = "phone";
-	public static final String isHaveCard = "is-have-card";
-	public static final String card = "card";
+	public static final String TYPE = "type";
+	public static final String FIRST_NAME = "first-name";
+	public static final String LAST_NAME = "last-name";
+	public static final String PHONE = "phone";
+	public static final String IS_HAVE_CARD = "is-have-card";
+	public static final String CARD = "card";
 	
 }
