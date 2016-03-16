367a6c7b6434d6e358aebb470f64ffa574f56381
Jurrio <jurimik@yandex.ua>
2016-03-16 08:55:33 +0200

    created class PersonService

---
+++ Salon/src/com/jurimik/service/PersonService.java
@@ -0,0 +1,24 @@
+package com.jurimik.service;
+
+import java.util.List;
+
+import com.jurimik.dao.PersonRepository;
+import com.jurimik.model.person.Person;
+
+public class PersonService {
+	public static boolean add(Person person){
+		return PersonRepository.add(person);
+	}
+	
+	public static Person find(int id) {
+		return PersonRepository.getById(id);
+	}
+	
+	public static List<Person> getAll() {
+		return PersonRepository.getAll();
+	}
+	
+	public static boolean delete(int id) {
+		return PersonRepository.delete(id);
+	}
+}
