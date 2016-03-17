269524e9d98ca8297cc852ee6e93e0412325c209
Jurrio <jurimik@yandex.ua>
2016-03-17 10:07:50 +0200

    improved method isPrint in Checker

--- Salon/src/com/jurimik/util/Checker.java
+++ Salon/src/com/jurimik/util/Checker.java
@@ -6,9 +6,7 @@ import com.jurimik.model.person.Employee;
 
 public class Checker {
 	public static boolean isPrint(Person person, boolean showClients, boolean showEmployees) {
-		if (person instanceof Client && showClients) 
-			return true;
-		if (person instanceof Employee && showEmployees)
+		if (person instanceof Client && showClients || person instanceof Employee && showEmployees) 
 			return true;
 		return false;
 	}
